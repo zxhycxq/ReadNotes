efe45b6c4fbf84555a1f2e87e82839943b07fc74
zxhycxq <lcyxlqn@163.com>
2017-09-12 18:33:37 +0800

    day6

--- Book-1/Demo/TODO/todoList/inideex.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Administrator on 2017/9/11 0011.
- * 九月
- */
---
+++ Book-1/demo/TODO/todoList/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/9/11 0011.
+ * 九月
+ */
+import * as actions from './actions';
+import reducer from './reducer';
+import view  from './views/container';
+
+export {actions,reducer,view}
+
